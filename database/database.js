2796dc166fbdabfd604837583e3e55b4c890577c
Tony Santos Costa <33641618+costatony79@users.noreply.github.com>
2023-07-08 15:53:59 -0400

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 //conexão com o banco de dados do mysql
-const connection = new Sequelize("trabalho_sexto_ano", "root", "admin", {
+const connection = new Sequelize("trabalho_sexto_ano", "aluno", "aluno", {
     host: "localhost",
     dialect: "mysql"
 });
